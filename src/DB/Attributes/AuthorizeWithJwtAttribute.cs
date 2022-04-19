a1db687f09dc0a9ef956d189826b690ed718e517
SalavatKhR <SalavatKhR@gmail.com>
2022-04-19 22:41:39 +0300

    AuthorizeWithJwtAttribute.cs added

---
+++ src/DB/Attributes/AuthorizeWithJwtAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using OpenIddict.Validation.AspNetCore;
+
+namespace DB.Attributes;
+
+public class AuthorizeWithJwtAttribute : AuthorizeAttribute
+{
+    public AuthorizeWithJwtAttribute()
+    { 
+        AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;
+    }
+}
