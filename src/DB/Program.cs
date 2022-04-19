a43818339c9c39da5a56fec458644076a5efacb5
SalavatKhR <SalavatKhR@gmail.com>
2022-04-19 21:59:47 +0300

    set-up authentication and privacy

--- src/DB/Program.cs
+++ src/DB/Program.cs
@@ -2,20 +2,86 @@ using System.Text.Json.Serialization;
 using DB.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using OpenIddict.Abstractions;
+using static Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<SpotifyContext>(options =>
-    options.UseNpgsql(connectionString));
+{
+    options.UseNpgsql(connectionString);
+    options.UseOpenIddict();
+});
 builder.Services.AddIdentity<UserInfo, IdentityRole>()
-    .AddEntityFrameworkStores<SpotifyContext>();
+    .AddEntityFrameworkStores<SpotifyContext>()
+    .AddDefaultTokenProviders();
+builder.Services.Configure<IdentityOptions>(options =>    //claims настрою при реализации авторизации
+{
+    options.ClaimsIdentity.UserNameClaimType = OpenIddictConstants.Claims.Name;
+    options.ClaimsIdentity.UserIdClaimType = OpenIddictConstants.Claims.Subject;
+    options.ClaimsIdentity.RoleClaimType = OpenIddictConstants.Claims.Role;
+    options.ClaimsIdentity.EmailClaimType = OpenIddictConstants.Claims.Email;
+});
+
+builder.Services.AddCors(opt =>
+        {
+            opt.AddDefaultPolicy(policyBuilder =>
+            {
+                policyBuilder            //Позже настроим CORS на принятие запросов только с нашего приложения
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
+
+builder.Services.AddAuthentication(configureOptions =>
+    {
+        configureOptions.DefaultAuthenticateScheme = AuthenticationScheme;
+        configureOptions.DefaultChallengeScheme = AuthenticationScheme;
+    })
+    .AddJwtBearer(options => { options.ClaimsIssuer = AuthenticationScheme; });
+builder.Services.AddAuthorization();
+
+
+builder.Services.AddOpenIddict()
+    .AddCore(coreOptions =>
+    {
+        coreOptions
+            .UseEntityFrameworkCore()
+            .UseDbContext<SpotifyContext>();
+    })
+    .AddServer(serverOptions =>
+    {
+        serverOptions
+            .AcceptAnonymousClients()
+            .AllowPasswordFlow()
+            .AllowRefreshTokenFlow();
+
+        serverOptions
+            .SetTokenEndpointUris("/authenticate"); //метод аутентификации и выдачи JWT
+
+        var cfg = serverOptions.UseAspNetCore();
+        if (builder.Environment.IsDevelopment() || builder.Environment.IsStaging())
+        {
+            cfg.DisableTransportSecurityRequirement();   
+        }
+        cfg.EnableTokenEndpointPassthrough();
+
+        serverOptions
+            .AddDevelopmentEncryptionCertificate()
+            .AddDevelopmentSigningCertificate();
+    })
+    .AddValidation(validationOption =>
+    {
+        validationOption.UseAspNetCore();
+        validationOption.UseLocalServer();
+    });
+
 builder.Services.AddControllers().AddJsonOptions(x =>
     x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -25,10 +91,21 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app
+    .UseStaticFiles()
+    .UseRouting()
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseCors()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapDefaultControllerRoute();
+    });
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
