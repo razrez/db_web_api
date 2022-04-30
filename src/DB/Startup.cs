f12ff987aa5daba79595cd0afb6f41439d7ea042
SalavatKhR <SalavatKhR@gmail.com>
2022-04-30 15:24:44 +0300

    Divided Program.cs into Startup.cs and StartupExtensions.cs

---
+++ src/DB/Startup.cs
@@ -0,0 +1,77 @@
+﻿using DB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DB;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+    private readonly IWebHostEnvironment _env;
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    {
+        _configuration = configuration;
+        _env = env;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddCors(opt =>
+        {
+            opt.AddDefaultPolicy(builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
+        services.AddAuthenticationAndJwt()
+            .AddAuthorization()
+            .AddOpenIddictServer(_env);
+        services.AddDbContext<SpotifyContext>(options =>
+        {
+            options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+            options.UseOpenIddict();
+        });
+        services.AddIdentity();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app
+            .UseStaticFiles()
+            .UseRouting()
+            .UseAuthentication()
+            .UseAuthorization()
+            .UseCors()
+            .UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host
+            .CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, builder) => builder
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true)
+                .AddEnvironmentVariables())
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+    public static void RunApp(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+}
---
+++ src/DB/StartupExtensions.cs
@@ -0,0 +1,81 @@
+﻿using DB.Models;
+using Microsoft.AspNetCore.Identity;
+using OpenIddict.Abstractions;
+using static Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults;
+
+namespace DB;
+
+public static class StartupExtensions
+{
+    public static IServiceCollection AddAuthenticationAndJwt(this IServiceCollection sc)
+    {
+        sc.AddAuthentication(configureOptions =>
+            {
+                configureOptions.DefaultAuthenticateScheme = AuthenticationScheme;
+                configureOptions.DefaultChallengeScheme = AuthenticationScheme;
+            })
+            .AddJwtBearer(options => { options.ClaimsIssuer = AuthenticationScheme; });
+        return sc;
+    }
+    
+    public static OpenIddictBuilder AddOpenIddictServer(this IServiceCollection services, 
+        IWebHostEnvironment environment)
+    {
+        return services
+            .AddOpenIddict()
+            .AddCore(options =>
+            {
+                options
+                    .UseEntityFrameworkCore()
+                    .UseDbContext<SpotifyContext>();
+            })
+            .AddServer(options =>
+            {
+                options
+                    .AcceptAnonymousClients()
+                    .AllowPasswordFlow()
+                    .AllowRefreshTokenFlow();
+
+                options
+                    .SetTokenEndpointUris("/signup", "/login");
+                
+                // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+                var cfg = options.UseAspNetCore();
+                if (environment.IsDevelopment() || environment.IsStaging())
+                {
+                    cfg.DisableTransportSecurityRequirement();   
+                }
+                
+                cfg.EnableTokenEndpointPassthrough();
+                
+                options
+                    .AddDevelopmentEncryptionCertificate()
+                    .AddDevelopmentSigningCertificate();
+            }).AddValidation(options =>
+            {
+                options.UseAspNetCore();
+                options.UseLocalServer();
+            });
+    }
+    
+    public static IServiceCollection AddIdentity(this IServiceCollection services)
+    {
+        services
+            .AddIdentity<UserInfo, IdentityRole>()
+            .AddEntityFrameworkStores<SpotifyContext>()
+            .AddDefaultTokenProviders();
+        
+        // Configure Identity to use the same JWT claims as OpenIddict instead
+        // of the legacy WS-Federation claims it uses by default (ClaimTypes),
+        // which saves you from doing the mapping in your authorization controller.
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.ClaimsIdentity.UserNameClaimType = OpenIddictConstants.Claims.Name;
+            options.ClaimsIdentity.UserIdClaimType = OpenIddictConstants.Claims.Subject;
+            options.ClaimsIdentity.RoleClaimType = OpenIddictConstants.Claims.Role;
+            options.ClaimsIdentity.EmailClaimType = OpenIddictConstants.Claims.Email;
+        });
+        
+        return services;
+    }
+}
