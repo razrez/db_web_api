cde88623e2750ed46abe6dc041d83d6427c63c3a
Руслан Русланов <sharp_blade666@mail.ru>
2022-05-04 22:40:01 +0300

    getResponseAsync

--- src/DB.Tests/UserContentTests.fs
+++ src/DB.Tests/UserContentTests.fs
@@ -13,17 +13,19 @@ type song = {id: int; userId: string; name:string; source: string}
 type playlist = { id: int; userId: string; title: string; playlistType:int; songs:List<song> }
 type responsePlaylists = List<playlist>
 
-
+let getResponseAsync path=
+    let factory = new WebApplicationFactory<Startup>()
+    let client = factory.CreateClient()
+    client.GetAsync($"{path}")
+    
 [<Theory>]
 [<InlineData("5f34130c-2ed9-4c83-a600-e474e8f48bac","user01@gmail.com")>]
 [<InlineData("5f34130c-2ed9-4c83-a600-e474e8f43bac","user03@gmail.com")>]
 [<InlineData("5f34130c-2ed9-4c83-a600-e474e8f44bac","user04@gmail.com")>]
 let ``GetUserName returns real user's name by id 5f34130c-2ed9-4c83-a600-e474e8f48bac`` (id:string, expected:string) =
-    let _factory = new WebApplicationFactory<Startup>()
-    let client = _factory.CreateClient();
+    let path = $"/UserContent/name/user/{id}"
     
-    //let id = "5f34130c-2ed9-4c83-a600-e474e8f48bac"
-    let response = client.GetAsync($"/UserContent/name/user/{id}")
+    let response = getResponseAsync path
     Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
     
     let responseJson = response.Result.Content.ReadAsStringAsync().Result
@@ -33,42 +35,41 @@ let ``GetUserName returns real user's name by id 5f34130c-2ed9-4c83-a600-e474e8f
 
 [<Fact>]
 let ``GetUserName returns NotFound with description`` () =
-    let _factory = new WebApplicationFactory<Startup>()
-    let client = _factory.CreateClient();
-    
     let id = "bad id"
-    let response = client.GetAsync($"/UserContent/name/user/{id}")
+    let path = $"/UserContent/name/user/{id}"
+    
+    let response = getResponseAsync path
     Assert.Equal(HttpStatusCode.NotFound, response.Result.StatusCode)
+    
     let responseJson = response.Result.Content.ReadAsStringAsync().Result
     let responseData = JsonSerializer.Deserialize<responseNotFound> responseJson
     Assert.Equal("Unexpected id",responseData.error)
     
 [<Fact>]
 let ``GetPlaylists returns NotFound with description`` () =
-    let _factory = new WebApplicationFactory<Startup>()
-    let client = _factory.CreateClient();
-    
     let id = "bad id"
-    let response = client.GetAsync($"/UserContent/playlists/user/{id}")
+    let path = $"/UserContent/playlists/user/{id}"
+    
+    let response = getResponseAsync path
     Assert.Equal(HttpStatusCode.NotFound, response.Result.StatusCode)
+    
     let responseJson = response.Result.Content.ReadAsStringAsync().Result
     let responseData = JsonSerializer.Deserialize<responseNotFound> responseJson
     Assert.Equal("Unexpected id",responseData.error)
     
 [<Fact>]
 let ``GetPlaylists returns not empty user's playlists by id 5f34130c-2ed9-4c83-a600-e474e8f48bac`` () =
-    let _factory = new WebApplicationFactory<Startup>()
-    let client = _factory.CreateClient();
-    
     let id = "5f34130c-2ed9-4c83-a600-e474e8f48bac"
-    let response = client.GetAsync($"/UserContent/playlists/user/{id}")
+    let path = $"/UserContent/playlists/user/{id}"
     
+    let response = getResponseAsync path
     Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
-    let responseJson = response.Result.Content.ReadAsStringAsync().Result
     
+    let responseJson = response.Result.Content.ReadAsStringAsync().Result
     Assert.NotEmpty(responseJson)
     
     let responseData = JsonSerializer.Deserialize<responsePlaylists> responseJson
     Assert.Equal(2,responseData.Count)
+    
     
     
