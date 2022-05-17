d75f17b43ad5ec020636acdc11da40765e98a5e6
Руслан Русланов <sharp_blade666@mail.ru>
2022-05-17 20:27:50 +0300

    GetPlaylist by id return Playlist

--- src/DB.Tests/PlaylistTests.fs
+++ src/DB.Tests/PlaylistTests.fs
@@ -1,19 +1,20 @@
-﻿module DB.Tests.PlaylistTests
+﻿
+module DB.Tests.PlaylistTests
 
-open System
-open System.Linq.Expressions
 open System.Net
-open DB.Data
-open DB.Data.Repository
-open Moq
-open Microsoft.AspNetCore.Mvc.Testing
-open Moq.EntityFrameworkCore
-open Xunit
+open System.Text.Json
 open DB
+open DB.Controllers
+open DB.Data.Repository
+open DB.Models
+open Microsoft.AspNetCore.Mvc
+open Microsoft.AspNetCore.Mvc.Testing
+open Moq
+open Moq.FSharp.Extensions
+open Xunit
+open DB.Tests.GetResponse
 
-type responseName = { name: string }
-type responseNotFound = { error: string }
-type Moq.Mock<'T> when 'T : not struct with
+(*type Moq.Mock<'T> when 'T : not struct with
           /// Specifies a setup on the mocked type for a call to a function
           member mock.SetupFunc<'TResult>(expression:Expression<Func<'T,'TResult>>) =
             mock.Setup<'TResult>(expression)
@@ -23,25 +24,54 @@ type Moq.Mock<'T> when 'T : not struct with
           /// Specifies a setup on the mocked type for a call to a property setter
           member mock.SetupSetAction<'TProperty>(setupExpression:Action<'T>) 
             : Moq.Language.Flow.ISetupSetter<'T,'TProperty> = 
-            mock.SetupSet<'TProperty>(setupExpression)
-            
-let deleteResponseAsync path=
-    let factory = new WebApplicationFactory<Startup>()
-    let client = factory.CreateClient()
-    client.DeleteAsync($"{path}")
+            mock.SetupSet<'TProperty>(setupExpression)*)
+type IRepository =
+    inherit ISpotifyRepository
     
-type Container<'a> = {repo: 'a;}
-
+type song = {id: int; userId: string; name:string; source: string}
+type playlist = { id: int; userId: string; title: string; playlistType:int; genreType:int; songs:List<song> }
+    
 [<Fact>]
 let ``Delete nonexistent Playlist and get NotFound`` () =
     let id = 234567892
     
-    let mock = Mock.Of<ISpotifyRepository>().DeletePlaylist(id).Result
-    Assert.False(mock)
-    (*let repMoq = Moq.Mock<ISpotifyRepository>()
-                     .SetupFunc<bool>(fun f -> f.DeletePlaylist(It.IsAny<int>()).Result).Returns(false)*)
     let path id = $"/api/playlist/{id}"
     let response = deleteResponseAsync path
     Assert.Equal(HttpStatusCode.NotFound, response.Result.StatusCode)
-
     
+    let moq = Mock<IRepository>()
+    moq.SetupFunc<bool>(fun f -> f.DeletePlaylist(It.IsAny<int>()).Result).Returns(false) |> ignore
+    
+    Assert.False(moq.Object.DeletePlaylist(id).Result)
+    let playlistController = PlaylistController(moq.Object)
+    
+    let myActionResult = playlistController.DeletePlaylist(id).Result
+    Assert.IsType<NotFoundObjectResult>(myActionResult)
+    
+[<Fact>]
+let ``Delete existent Playlist and get Ok`` () =
+    let id = 2
+    
+    let moq = Mock<IRepository>()
+    moq.SetupFunc<bool>(fun f -> f.DeletePlaylist(It.IsAny<int>()).Result).Returns(true) |> ignore
+    
+    Assert.True(moq.Object.DeletePlaylist(id).Result)
+    let playlistController = PlaylistController(moq.Object)
+    
+    let myActionResult = playlistController.DeletePlaylist(id).Result
+    Assert.IsType<OkResult>(myActionResult)
+    
+[<Fact>]
+let ``GetPlaylist by id return Playlist``() =
+    let id = 1
+    let factory = new WebApplicationFactory<Startup>()
+    let client = factory.CreateClient()
+    
+    let path = $"/api/playlist/{id}"
+    let response = client.GetAsync($"{path}")
+    Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode)
+    
+    let responseJson = response.Result.Content.ReadAsStringAsync().Result
+    Assert.NotEmpty(responseJson)
+    let responseData = JsonSerializer.Deserialize<playlist> responseJson
+    Assert.Equal("LikedSongs", responseData.title)
