ae2d0859371164e4a3d01be25e14887273a7c8ad
SalavatKhR <SalavatKhR@gmail.com>
2022-06-04 01:00:43 +0300

    Added getting pictures

--- src/DB/Controllers/FileController.cs
+++ src/DB/Controllers/FileController.cs
@@ -9,23 +9,52 @@ namespace DB.Controllers;
 public class FileController : ControllerBase
 {
     private readonly ISpotifyRepository _context;
-    private readonly IWebHostEnvironment _env;
+    private static string _directoryPath = null!;
 
     public FileController(IWebHostEnvironment env, ISpotifyRepository context)
     {
-        _env = env;
         _context = context;
+        _directoryPath = Path.Combine(env.ContentRootPath, "..\\..\\files\\");
     }
 
-    [HttpGet("/song")]
+    [HttpGet("song")]
     public IActionResult GetSongFile(int songId)
     {
-        var root = _env.ContentRootPath;
-        var directory = Path.Combine(root, "..\\..\\files\\songs\\");
-        var songSource = _context.GetSong(songId).Result.Source;
-        var songPath = Path.Combine(directory, songSource);
+        /*var songSource = _context.GetSong(songId).Result.Source;
+        var songPath = Path.Combine(_directoryPath, $"songs\\{songSource}");
         var song = System.IO.File.ReadAllBytes(songPath);
-        Response.Headers.Add("Content-Disposition", $"filename=\"{songSource}\"");
-        return File(song, "audio/mpeg", songSource);
+        return File(song, "audio/mpeg", songSource);*/
+        
+        var song = _context.GetSong(songId).Result;
+        if(song == null)
+            return BadRequest("Wrong song ID");
+        var songSrc = song.Source;
+        var songPath = Path.Combine(_directoryPath, $"songs\\{songSrc}");
+        var img = System.IO.File.ReadAllBytes(songPath);
+        return File(img, "audio/mpeg", songSrc);
+    }
+    
+    [HttpGet("picture/user")]
+    public IActionResult GetUserPicture(string userId)
+    {
+        var profile = _context.GetProfile(userId).Result;
+        if(profile == null)
+            return BadRequest("Wrong user ID");
+        var profileImgSrc = profile.ProfileImg ?? "user_test.jpg";
+        var imgPath = Path.Combine(_directoryPath, $"pictures\\users\\{profileImgSrc}");
+        var img = System.IO.File.ReadAllBytes(imgPath);
+        return File(img, "image/jpeg", profileImgSrc);
+    }
+    
+    [HttpGet("picture/playlist")]
+    public IActionResult GetPlaylistPicture(int playlistId)
+    {
+        var playlist = _context.GetPlaylistInfo(playlistId).Result;
+        if (playlist == null)
+            return BadRequest("Wrong playlist ID");
+        var playlistImgSrc = playlist.ImgSrc ?? "playlist_test.jpg";
+        var imgPath = Path.Combine(_directoryPath, $"pictures\\playlists\\{playlistImgSrc}");
+        var img = System.IO.File.ReadAllBytes(imgPath);
+        return File(img, "image/jpeg", playlistImgSrc);
     }
 }
