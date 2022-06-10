6d4c4d29020683aaf47cffb067bb69ed478b4407
SalavatKhR <SalavatKhR@gmail.com>
2022-06-10 23:44:36 +0300

    added Premium.cs)))

---
+++ src/DB/Models/Premium.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace DB.Models;
+
+[Table("premium")]
+public class Premium
+{
+    public Premium()
+    {
+        UserPremiums = new HashSet<UserPremium>();
+    }
+    
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("name")]
+    public string Name { get; set; } = null!;
+    [Column("description")]
+    public string Description { get; set; } = null!;
+    [Column("price")]
+    public double Price { get; set; }
+    [DefaultValue(1)]
+    [Column("users_count")]
+    public  int UserCount { get; set; }
+    
+    [JsonIgnore]
+    public ICollection<UserPremium> UserPremiums { get; set; }
+}
