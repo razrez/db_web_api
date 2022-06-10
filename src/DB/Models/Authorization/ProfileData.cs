5099d8fc60865196137b64ba59b0eea95bb52a36
Daniil <dddedashev@mail.ru>
2022-06-10 17:30:38 +0300

    fix warn second

--- src/DB/Models/Authorization/ProfileData.cs
+++ src/DB/Models/Authorization/ProfileData.cs
@@ -4,7 +4,7 @@ namespace DB.Models.Authorization;
 
 public class ProfileData
 {
-    public string? Name { get; set; }
+    public string Name { get; set; }
     public int BirthYear { get; set; }
     public int BirthMonth { get; set; }
     public int BirthDay { get; set; }
